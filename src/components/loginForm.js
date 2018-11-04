ef3231b071868c69509e1f4428893762121b6d63
Chacko <chackop@gmail.com>
2018-11-04 23:09:42 +0000

    added firebase

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 import { Button, FormInput } from 'react-native-elements';
 import InnerSection from "./inner-section";
+import firebase from 'firebase';
+
+componentDidMount = () => {
+    const config = {
+        apiKey: "AIzaSyCOg_WZIBgjDOKkuGmRK8SZ29st1tqNMNo",
+        authDomain: "rcnauthideapad.firebaseapp.com",
+        databaseURL: "https://rcnauthideapad.firebaseio.com",
+        projectId: "rcnauthideapad",
+        storageBucket: "rcnauthideapad.appspot.com",
+        messagingSenderId: "773038312425"
+      };
+      firebase.initializeApp(config);
+};
+
 
 export default class LoginForm extends Component {
   render() {
@@ -15,7 +29,7 @@ export default class LoginForm extends Component {
       <InnerSection>
         <FormInput placeholder="Password" secureTextEntry={true} />
       </InnerSection>
-      
+
         <Button title="Login" backgroundColor="red" />
       </View>
     )
