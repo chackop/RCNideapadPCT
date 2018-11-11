1f274a0d967c9ea69d5ea7e0c32ca7b0594cd34d
Chacko <chackop@gmail.com>
2018-11-11 15:25:59 +0000

    ideallist nav

--- src/navigation.js
+++ src/navigation.js
@@ -3,6 +3,7 @@ import { StackNavigator, SwitchNavigator } from "react-navigation";
 import LoginForm from "./components/loginForm";
 import IdeaList from "./components/idealist";
 import IdeaPadForm from "./components/ideapadForm";
+import { Icon } from 'react-native-elements';
 
 const AuthStack = StackNavigator({
     Login: {
@@ -14,16 +15,28 @@ const AuthStack = StackNavigator({
 });
 
 const AppStack = StackNavigator({
+    Ideas: {
+        screen: IdeaList,
+        navigationOptions: ({navigation}) => {
+            return {
+                title: 'Your IdeaPad',
+                headerRight: (
+                    <Icon 
+                        type= 'evilicon'
+                        name= 'plus'
+                        size= {30}
+                        onPress= {() => navigation.navigate('AddIdeas')}
+                        iconStyle={{ paddingRight: 10 }}
+                    />
+                ),
+                headerLeft: null
+            }
+        }
+    },
     AddIdeas: {
         screen: IdeaPadForm,
         navigationOptions: {
             headerTitle: 'Idea Pad form'
-        }
-    },
-    Ideas: {
-        screen: IdeaList,
-        navigationOptions: {
-            headerTitle: 'Idea List'
         }
     }
 });
